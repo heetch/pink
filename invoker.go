89fda03d6310f2796896b8e91bdf5131bc6a2c5d
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2019-01-30 11:46:20 +0100

    Implement ExecutableInvoker

--- invoker.go
+++ invoker.go
@@ -1,6 +1,11 @@
 package pink
 
-import "context"
+import (
+	"context"
+	"os"
+	"os/exec"
+	"path"
+)
 
 // Invoker invokes a plugin and passes it the given configuration.
 type Invoker interface {
@@ -11,4 +16,23 @@ type Invoker interface {
 type InvokerConfig struct {
 	Args []string
 	Env  []string
+}
+
+// ExecutableInvoker invokes a plugin as an executable.
+type ExecutableInvoker struct {
+	PluginDir string
+}
+
+// Invoke an executable described by the given manifest. The configuration can be used to
+// pass args and environment variables to that executable.
+func (e *ExecutableInvoker) Invoke(ctx context.Context, m *Manifest, cfg *InvokerConfig) error {
+	execPath := path.Join(e.PluginDir, path.Join(m.Command...), m.Path)
+
+	cmd := exec.CommandContext(ctx, execPath)
+	cmd.Env = cfg.Env
+	cmd.Args = cfg.Args
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	return cmd.Run()
 }
