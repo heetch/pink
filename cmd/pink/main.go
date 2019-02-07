869a41da2dc3cfa822f52cf905e8aea29b0590f8
Geoffrey John Teale <tealeg@gmail.com>
2019-02-07 11:09:23 +0100

    Add main function

---
+++ cmd/pink/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/heetch/pink"
+)
+
+// getPinkDir returns the default state directory for pink
+func getPinkDir() string {
+	return filepath.Join(os.Getenv("HOME"), ".pink")
+}
+
+// ensurePinkDirs will create the local filestructure if it doesn't exist
+func ensurePinkDirs(root string) error {
+	return os.MkdirAll(pink.GetPluginsDir(root), 0700)
+
+}
+
+func main() {
+	pd := getPinkDir()
+	if err := ensurePinkDirs(pd); err != nil {
+		log.Fatalf(err.Error())
+	}
+	plugD := pink.GetPluginsDir(pd)
+	// If we don't pass any arguments default to running help on
+	// the root of the plugins directory
+	if len(os.Args) == 1 {
+		pink.Help(plugD)
+		return
+	}
+
+	path, args, runHelp, err := pink.DispatchCommand(os.Args[1:], []string{}, plugD)
+	if err != nil {
+		log.Fatalf(err.Error())
+		return
+	}
+	if runHelp {
+		pink.Help(path)
+		return
+	}
+	fmt.Printf("%q - %q\n", path, args)
+}
