8ca040d0f81af859e9da462b09c9a6b25752d677
Geoffrey John Teale <tealeg@gmail.com>
2019-02-11 15:52:16 +0100

    Test invokation via the Manifest

--- manifest_test.go
+++ manifest_test.go
@@ -1,9 +1,14 @@
 package pink
 
 import (
+	"context"
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/exec"
+	"path"
+	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -53,4 +58,47 @@ func TestValidateManifest(t *testing.T) {
 		err := validateManifest(&test.m)
 		require.Equal(t, test.returnsError, err != nil)
 	}
+}
+
+// Invoke defers to the correct invoker
+func TestInvoke(t *testing.T) {
+	wd, err := os.Getwd()
+	require.NoError(t, err)
+
+	t.Run("executable", func(t *testing.T) {
+		execCommandContext = func(ctx context.Context, command string, args ...string) *exec.Cmd {
+			require.Equal(t, path.Join(wd, "a/b/somepath"), command)
+			return &exec.Cmd{Path: "/bin/echo"}
+		}
+		defer func() { execCommandContext = exec.CommandContext }()
+
+		m := Manifest{Command: []string{"a", "b"}, Invoker: "executable", Exec: "somepath"}
+
+		require.NoError(t, err)
+		err = m.Invoke(context.Background(), wd, nil)
+		require.NoError(t, err)
+
+	})
+
+	tmpDir, err := ioutil.TempDir("", "")
+	require.NoError(t, err)
+	tmpPath := filepath.Join(tmpDir, "stdout")
+	tmpFile, err := os.Create(tmpPath)
+	require.NoError(t, err)
+	origStdout := os.Stdout
+	os.Stdout = tmpFile
+	defer func() { os.Stdout = origStdout }()
+
+	m := Manifest{Invoker: "docker", Docker: DockerConfig{ImageURL: "alpine"}}
+	err = m.Invoke(context.Background(), wd, []string{"echo", "hello", "world"})
+	require.NoError(t, err)
+
+	tmpFile.Sync()
+	tmpFile.Close()
+
+	b, err := ioutil.ReadFile(tmpPath)
+	require.NoError(t, err)
+
+	require.Equal(t, "hello world", strings.TrimSpace(string(b)))
+
 }
